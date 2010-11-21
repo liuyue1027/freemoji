e10ef0304d8bcc483577f5c5477689ff42fbfd7c
lvyile <lvyile@cocoachina.com>
2010-11-21 23:52:20 +0800

    update for new files

---
+++ FreemojiController.h
@@ -0,0 +1,16 @@
+//
+//  FreemojiController.h
+//  Freemoji
+//
+//  Created by shang taotao on 10-11-21.
+//  Copyright 2010年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FreemojiController : UIViewController {
+    
+}
+
+@end
