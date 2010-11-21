88c68b3c58f03b8e20096ef9d7d84dd485bd4f95
lvyile <lvyile@cocoachina.com>
2010-11-21 23:06:49 +0800

    add AppDelegate

---
+++ Freemoji.xcodeproj/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Freemoji
+//
+//  Created by lv on 10-11-21.
+//  Copyright 2010年 CocoaChina.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AppDelegate : NSObject {
+
+}
+
+@end
---
+++ Freemoji.xcodeproj/AppDelegate.m
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.m
+//  Freemoji
+//
+//  Created by lv on 10-11-21.
+//  Copyright 2010年 CocoaChina.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+@implementation AppDelegate
+
+@end
